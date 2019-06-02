b4ca7cdb0633328db8646403bf9664ac90abcc49
Leo.Liu <xinliu.leo@gmail.com>
2019-06-03 09:47:13 +1000

    trunk

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Leo Liu on 30/5/19.
+//  Copyright © 2019 Leo Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
